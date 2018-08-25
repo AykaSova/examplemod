a8818b783bba9781d88c9926e25d0704e037f9b3
Айка <ayka-sova@rambler.ru>
2018-08-25 09:27:05 +0300

    Add files via upload

---
+++ Demo1.java
@@ -0,0 +1,25 @@
+package com.Forge.Mod1;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.common.MinecraftForge;
+
+@Mod(modid = Demo1.MODID, name = Demo1.NAME, version = Demo1.VERSION)
+
+public class Demo1 {
+	public static final String MODID = "Mod3";
+	public static final String NAME = "MyFirstMod";
+	public static final String VERSION = "1.0";
+
+	@EventHandler
+	public void init(FMLInitializationEvent event)
+	{
+		//System.out.println("Hello, world!");
+		MinecraftForge.EVENT_BUS.register(new BlockBreaker());
+		MinecraftForge.EVENT_BUS.register(new Cart());
+		MinecraftForge.EVENT_BUS.register(new Living());		
+	}
+
+}
+ 
